ca6bd99706df527ca92f2ba0a75d18435f59c4ba
ladykay1209 <tkz.mhlanga@gmail.com>
2026-07-15 23:08:06 +0200

    Add events data for upcoming services and meetings
    Signed-off-by: ladykay1209 <tkz.mhlanga@gmail.com>

---
+++ js/events.js
@@ -0,0 +1,13 @@
+const events = {
+    '2026-07-15T18:00:00': {
+        title: 'Youth Prayer Service',
+        venue: 'LKP Presbytery Hall',
+        description: 'An evening of worship and prayer.'
+    },
+
+    '2026-08-01T09:00:00': {
+        title: 'Senate Meeting',
+        venue: 'Mamelodi Congregation',
+        description: 'Presbytery meeting.'
+    }
+};
